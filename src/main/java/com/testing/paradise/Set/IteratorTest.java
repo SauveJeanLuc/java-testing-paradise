e84a82c15841244d2773373fe803f6c2d5c8a7ef
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-08-07 12:42:39 +0200

    Defined Iterator Class for Test

---
+++ src/main/java/com/testing/paradise/Set/IteratorTest.java
@@ -0,0 +1,19 @@
+package com.testing.paradise.Set;
+
+import lombok.*;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class IteratorTest {
+
+    private Set<Role> roles = new HashSet<>();
+
+
+}
