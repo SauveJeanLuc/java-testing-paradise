d32725d1ddfc60792509dd341d61ac2a17862d57
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-09-24 15:41:22 +0200

    Path

--- src/main/java/com/testing/paradise/Path/Pathy.java
+++ src/main/java/com/testing/paradise/Path/Pathy.java
@@ -5,14 +5,23 @@ import java.nio.file.Paths;
 
 public class Pathy {
     public static void main(String[] args) {
-        String path = "opt/etrainer/letters/NIYOGITARE PATRICK-44b5ddb166ca45b0a0f073619f1bd313.pdf";
+        String path ="/opt/etrainer/transcripts/Patrick Niyogitare Resume-6c2b114d1f1b4c94976cbd59484250b9.pdf";
         Path pathDTO = Paths.get(path);
-        Path directory = pathDTO.getParent();
-        Path fileName = pathDTO.getFileName();
-        String dir = String.format("/%s",directory);
-        System.out.println("PATHDTO: " + pathDTO.toString());
-        System.out.println("Directory: " + dir);
-        System.out.println("third component: " + pathDTO.subpath(2,3));
-        System.out.println(" File Name: " + fileName.toString());
+//        String directory = String.format("/%s",pathDTO.getParent());
+        String fileName = String.format("%s",pathDTO.getFileName());
+        String str = pathDTO.subpath(2,3).toString();
+        String dir = "";
+        String transferLettersDirectory ="SAUVE Jean-Luc";
+        switch(str)
+        {
+            case "transcripts":
+                dir = transferLettersDirectory;
+                break;
+            default:
+                dir = "nothing";
+        }
+        System.out.println(dir);
+        System.out.println(fileName);
+
     }
 }
