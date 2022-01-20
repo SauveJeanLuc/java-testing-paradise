608286a810fa06e2087fe2fc33276697622f61ef
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-20 06:28:34 +0200

    Read Through Document test passed

--- src/main/java/com/testing/paradise/Excel/ExcelTest.java
+++ src/main/java/com/testing/paradise/Excel/ExcelTest.java
@@ -1,8 +1,6 @@
 package com.testing.paradise.Excel;
 
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
@@ -20,6 +18,36 @@ public class ExcelTest {
         Sheet sheet = workbook.getSheetAt(0);
 
         Map<Integer, List<String>> data = new HashMap<>();
+
+        int i = 0;
+
+        for (Row row : sheet) {
+            data.put(i, new ArrayList<String>());
+            for (Cell cell : row) {
+                switch (cell.getCellTypeEnum()) {
+                    case STRING:
+                        data.get(new Integer(i)).add(cell.getRichStringCellValue().getString());
+                        break;
+                    case NUMERIC:
+                        if(DateUtil.isCellDateFormatted(cell)){
+                            data.get(new Integer(i)).add(cell.getDateCellValue() + "");
+                        } else {
+                            data.get(new Integer(i)).add(cell.getNumericCellValue() + "");
+                        }
+                        break;
+                    case BOOLEAN:
+                        data.get(new Integer(i)).add(cell.getBooleanCellValue() + "");
+                        break;
+                    default:
+                        data.get(new Integer(i)).add(" ");
+                }
+            }
+            i++;
+        }
+
+        System.out.println("================================");
+        System.out.println(data);
+        System.out.println("================================");
 
     }
 
