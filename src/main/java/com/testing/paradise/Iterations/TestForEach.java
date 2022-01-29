ccf2e1e2f8c0d6436645f5e38e31ef33cb894795
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-01-29 22:30:33 +0200

    Change derivatives counter

--- src/main/java/com/testing/paradise/Iterations/TestForEach.java
+++ src/main/java/com/testing/paradise/Iterations/TestForEach.java
@@ -5,10 +5,10 @@ public class TestForEach {
     public static void main(String[] args){
 
         String[] UTFDerivatives = {"CESU-8", "MySQL utf8mb3", "Modified UTF-8", "WTF-8", "PEP 383"};
-        Integer counter = 0;
+        Integer counter = 23;
 
         for (String derivative : UTFDerivatives){
-            System.out.println(String.format("%s. %s", counter, derivative));
+            System.out.println(String.format("%s - %s", counter, derivative));
             counter++;
         }
     }
