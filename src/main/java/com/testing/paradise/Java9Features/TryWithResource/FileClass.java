6c4ccf7cafe0b7607307e59404d7f41c860736e6
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-02 23:14:31 +0200

    Initiate file for try-with-resource check

---
+++ src/main/java/com/testing/paradise/Java9Features/TryWithResource/FileClass.java
@@ -0,0 +1,13 @@
+package com.testing.paradise.Java9Features.TryWithResource;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class FileClass {
+
+    public static void main(String[] args)  throws  FileNotFoundException{
+        // Resource can be declared outside the try-with-resource
+        // Compile Error is Removed in Java 9
+
+
+    }
+}
