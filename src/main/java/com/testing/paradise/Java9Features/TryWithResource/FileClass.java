c9f3ce0483dbc2199464ab499d3e45440b6b16db
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-02 23:21:09 +0200

    write in file with resource declared outside try-with-resource

--- src/main/java/com/testing/paradise/Java9Features/TryWithResource/FileClass.java
+++ src/main/java/com/testing/paradise/Java9Features/TryWithResource/FileClass.java
@@ -1,13 +1,24 @@
 package com.testing.paradise.Java9Features.TryWithResource;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FileClass {
 
-    public static void main(String[] args)  throws  FileNotFoundException{
+    public static void main(String[] args) throws IOException {
         // Resource can be declared outside the try-with-resource
         // Compile Error is Removed in Java 9
 
+        FileOutputStream fileStream = new FileOutputStream("tryresourcetest.txt");
+
+        try(fileStream){
+            String data = "Red Flag";
+            byte dataInBytes[] = data.getBytes();
+            fileStream.write(dataInBytes);
+            System.out.println("File written");
+        }catch (Exception e){
+            System.out.println(e);
+        }
 
     }
 }
