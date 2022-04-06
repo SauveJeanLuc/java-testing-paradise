d9c3e6e58246c97038a20a92e4db26ae7899fe70
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-06 23:33:17 +0200

    enqueue method

--- src/main/java/com/testing/paradise/DSA/QueueDemo.java
+++ src/main/java/com/testing/paradise/DSA/QueueDemo.java
@@ -7,6 +7,19 @@ public class QueueDemo {
     int[] arr;
 
     QueueDemo(){
-
+        front = rear = -1;
+        arr = new int[10];
     }
+
+    void enqueue(int a){
+        if(rear == arr.length-1)
+            System.out.println("overflow");
+        else
+            arr[++rear] = a;
+
+        if(front==-1)
+            front++;
+    }
+
+
 }
