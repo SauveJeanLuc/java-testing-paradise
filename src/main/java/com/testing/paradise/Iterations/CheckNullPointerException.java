ec46aa9877b294858ae1df9cf494ecc89af5bb76
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-07 22:31:37 +0200

    Check passe

---
+++ src/main/java/com/testing/paradise/Iterations/CheckNullPointerException.java
@@ -0,0 +1,15 @@
+package com.testing.paradise.Iterations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckNullPointerException {
+    public static void main(String[] args) {
+        List<String> forTest;
+
+        for(String tested : forTest){
+            System.out.println(tested);
+        }
+    }
+
+}
