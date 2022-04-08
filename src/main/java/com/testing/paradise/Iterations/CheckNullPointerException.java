7af2c02105977d4c0aec677c46d764c8032e0b10
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-08 13:14:27 +0200

    Refine CheckNullPointerException

--- src/main/java/com/testing/paradise/Iterations/CheckNullPointerException.java
+++ src/main/java/com/testing/paradise/Iterations/CheckNullPointerException.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class CheckNullPointerException {
     public static void main(String[] args) {
-        List<String> forTest;
+        List<String> forTest = new ArrayList<>();
 
         for(String tested : forTest){
             System.out.println(tested);
